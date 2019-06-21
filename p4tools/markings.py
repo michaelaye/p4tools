49a251ef78609140f8555310ac9b027cef052899
K.-Michael Aye <kmichael.aye@gmail.com>
2019-06-21 15:36:10 -0600

    adding Fan

--- p4tools/markings.py
+++ p4tools/markings.py
@@ -2,10 +2,13 @@ import math
 from math import cos, degrees, pi, radians, sin, tau
 from pathlib import Path
 
+import matplotlib.lines as lines
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.patches import Ellipse
+from numpy import arctan2
+from numpy import linalg as LA
 from shapely import affinity
 from shapely import geometry as geom
 
@@ -206,3 +209,257 @@ class Blotch(Ellipse):
 
     def __repr__(self):
         return self.__str__()
+
+
+def rotate_vector(v, angle):
+    """Rotate vector by angle given in degrees.
+
+    Parameters
+    ----------
+    v : np.array
+        Vector to be rotated
+    angle : float
+        Angle in degrees
+    """
+    rangle = radians(angle)
+    rotmat = np.array([[cos(rangle), -sin(rangle)],
+                       [sin(rangle), cos(rangle)]])
+    return rotmat.dot(v)
+
+
+class Fan(lines.Line2D):
+
+    """Fan management class for P4.
+
+    Parameters
+    ----------
+    data : object with fan data attributes
+        object has to provide [`x`, `y`, `angle`, `spread`, `distance`]
+    scope : {'planet4', 'hirise'}
+        string that decides between using x/y or image_x/image_y as base coords
+    kwargs : dictionary, optional
+
+    Attributes
+    ----------
+    to_average : list
+        List of columns to average after clustering
+    data : object with fan data attributes
+        as provided by `data`.
+    base : tuple
+        base coordinates `x` and `y`.
+    inside_half : float
+        `data` divided by 2.0.
+    armlength : float
+        length of the fan arms.
+    v1 : float[2]
+        vector of first arm of fan.
+    v2 : float[2]
+        vector of second arm of fan.
+    coords : float[3, 2]
+        Set of coords to draw for MPL.Line2D object: arm1->base->arm2
+    circle_base
+    center
+    radius
+    midpoint
+    base_to_midpoint_vec
+    """
+
+    to_average = 'x y image_x image_y angle spread distance'.split()
+
+    def __init__(self, data, scope='planet4', with_center=False, **kwargs):
+        self.data = data
+        self.scope = scope if scope is not None else 'planet4'
+        self.with_center = with_center
+        if scope not in ['hirise', 'planet4']:
+            raise TypeError('Unknown scope: {}'.format(scope))
+        # first coordinate is the base of fan
+        actual_x = 'x' if scope == 'planet4' else 'image_x'
+        actual_y = 'y' if scope == 'planet4' else 'image_y'
+        try:
+            self.base = self.data.loc[[
+                actual_x, actual_y]].values.astype('float')
+        except KeyError:
+            print("No x and y in the data:\n{}".format(data))
+            raise KeyError
+        # default n_members value (property)
+        self._n_members = 1
+        # angles
+        self.inside_half = self.data.spread / 2.0
+        alpha = self.data.angle - self.inside_half
+        beta = self.data.angle + self.inside_half
+        # length of arms
+        self.armlength = self.get_arm_length()
+        # first arm
+        self.v1 = rotate_vector([self.armlength, 0], alpha)
+        # second arm
+        self.v2 = rotate_vector([self.armlength, 0], beta)
+        # vector matrix, stows the 1D vectors row-wise
+        self.coords = np.vstack((self.base + self.v1,
+                                 self.base,
+                                 self.base + self.v2))
+        # init fan line, first column are the x-components of the row-vectors
+        lines.Line2D.__init__(self, self.coords[:, 0], self.coords[:, 1],
+                              alpha=0.65, color='white',
+                              **kwargs)
+
+    def is_equal(self, other):
+        if self.data.x == other.data.x and\
+           self.data.y == other.data.y and\
+           self.data.image_x == other.data.image_x and\
+           self.data.image_y == other.data.image_y and\
+           self.data.angle == other.data.angle and\
+           self.data.spread == other.data.spread and\
+           self.data.distance == other.data.distance:
+            return True
+        else:
+            return False
+
+    @property
+    def n_members(self):
+        return self._n_members
+
+    @n_members.setter
+    def n_members(self, value):
+        self._n_members = value
+
+    def get_arm_length(self):
+        half = radians(self.inside_half)
+        return self.data.distance / (cos(half) + sin(half))
+
+    @property
+    def area(self):
+        tr_h = np.sqrt(self.armlength**2 - self.radius**2)
+        tr_area = tr_h * self.radius
+        half_circ_area = 0.5 * pi * self.radius**2
+        return tr_area + half_circ_area
+
+    @property
+    def circle_base(self):
+        "float[2] : Vector between end of first arm and second arm of fan."
+        return self.v1 - self.v2
+
+    @property
+    def semi_circle_center(self):
+        """float[2] : vector from base to mid-point between end of arms.
+
+        This is used for the drawing of the semi-circle at the end of the
+        two fan arms.
+        """
+        return self.base + self.v2 + 0.5 * self.circle_base
+
+    @property
+    def radius(self):
+        "float : for the semi-circle wedge drawing at the end of fan."
+        return 0.5 * LA.norm(self.circle_base)
+
+    def add_semicircle(self, ax, color='b'):
+        "Draw a semi-circle at end of fan arms using MPL.Wedge."
+        # reverse order of arguments for arctan2 input requirements
+        theta1 = degrees(arctan2(*self.circle_base[::-1]))
+        theta2 = theta1 + 180
+        wedge = mpatches.Wedge(self.semi_circle_center, self.radius, theta1, theta2,
+                               width=0.01 * self.radius, color=color, alpha=0.65)
+        ax.add_patch(wedge)
+
+    def add_mean_wind_pointer(self, ax, color='b', ls='-'):
+        "Draw a thicker mean wind direction pointer for better visibility in plots."
+        endpoint = rotate_vector([3 * self.armlength, 0], self.data.angle)
+        coords = np.vstack((self.base,
+                            self.base + endpoint))
+        self.wind_pointer_end = coords[1]
+        pointer = lines.Line2D(coords[:, 0], coords[:, 1],
+                               alpha=0.65, linewidth=1, linestyle=ls)
+        pointer.set_color(color)
+        ax.add_line(pointer)
+
+    def plot(self, color=None, ax=None):
+        if ax is None:
+            _, ax = plt.subplots()
+            set_subframe_size(ax)
+        if color is not None:
+            self.set_color(color)
+        ax.add_line(self)
+        self.add_semicircle(ax, color=color)
+        if self.with_center:
+            self.plot_center(ax, color=color)
+
+    @property
+    def center(self):
+        """Calculate vector to half total length.
+
+        As total length, I define the armlength + the radius of the semi-circle
+        at the end.
+        """
+        mid_point_vec = rotate_vector([0.5 * (self.armlength + self.radius),
+                                       0],
+                                      self.data.angle)
+        return self.base + mid_point_vec
+
+    def plot_center(self, ax, color='b'):
+        ax.scatter(self.center[0], self.center[1], color=color,
+                   s=20, marker='.')
+
+    @property
+    def base_to_center_vec(self):
+        coords = np.vstack((self.base, self.center))
+        return coords
+
+    def add_midpoint_pointer(self, ax, color='b', ls='-'):
+        coords = self.base_to_center_vec
+        pointer = lines.Line2D(coords[:, 0], coords[:, 1],
+                               alpha=0.65, linewidth=3, linestyle=ls)
+        pointer.set_color(color)
+        ax.add_line(pointer)
+
+    def __str__(self):
+        out = "markings.Fan object. Input data:\n"
+
+        out += 'base: {0}\narmlength: {1}\narm1: {2}\narm2: {3}'\
+            .format(self.base, self.armlength, self.base + self.v1,
+                    self.base + self.v2)
+        return out
+
+    def __repr__(self):
+        return self.__str__()
+
+    def store(self, fpath=None):
+        out = self.data
+        for i, arm in enumerate([self.v1, self.v2]):
+            out['arm{}_x'.format(i + 1)] = (self.base + arm)[0]
+            out['arm{}_y'.format(i + 1)] = (self.base + arm)[1]
+        if 'image_id' not in out.index:
+            # out['image_id'] = self.image_id
+            raise AttributeError("Storage of image_id failed.")
+        if fpath is not None:
+            out.to_hdf(str(fpath.with_suffix('.hdf')), 'df')
+        out['n_members'] = self.n_members
+        return out
+
+    def to_shapely(self):
+        """Create a shapely half circle rotated by the fan's angle.
+
+        Notes
+        =====
+        `Motivated by: <https://stackoverflow.com/a/30762727/680232>`_
+        """
+        # Define the arc (presumably ezdxf uses a similar convention)
+        centerx, centery = self.semi_circle_center
+
+        # make a semi-circle first that points to the x-axis, rotate later.
+        start_angle = 270  # In degrees
+
+        # number of elements for the semi-circle
+        numsegments = 100
+
+        # The coordinates of the arc
+        theta = np.radians(np.linspace(start_angle, start_angle+180, numsegments))
+        x = centerx + self.radius * np.cos(theta)
+        y = centery + self.radius * np.sin(theta)
+
+        arc = geom.LineString(np.column_stack([x, y]))
+
+        rotated = affinity.rotate(arc, self.data.angle,
+                                  origin=tuple(self.semi_circle_center))
+
+        df = pd.DataFrame(np.vstack([self.coords[::-1][:2], np.array(rotated)]))
+        return geom.Polygon(df.round(2).drop_duplicates().values)
