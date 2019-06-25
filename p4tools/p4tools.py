34b92bd2b3eedb3e7bb925740b72b2aab2e4e229
K.-Michael Aye <kmichael.aye@gmail.com>
2019-06-25 14:29:02 -0700

    remove obsolete code

--- p4tools/p4tools.py
+++ p4tools/p4tools.py
@@ -1,11 +1,3 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-from importlib import resources
-import pandas as pd
-
-
-def get_region_names():
-    with resources.path("p4tools.data", "obsid_region_names.csv") as p:
-        return pd.read_csv(p)
-
