6f184efe3ae46ee33389c5c01ccbc535b62286ec
K.-Michael Aye <kmichael.aye@gmail.com>
2019-07-10 11:35:00 -0600

    fix Fan marking and add plot_fans_for_tile to plotting module

--- p4tools/markings.py
+++ p4tools/markings.py
@@ -1,9 +1,9 @@
 import math
-from math import cos, degrees, pi, radians, sin, tau
+from math import cos, degrees, pi, radians, sin
 from pathlib import Path
 
-import intake
 import matplotlib.lines as lines
+import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -32,7 +32,7 @@ def calc_fig_size(width):
 
 
 class Marking:
-    def __init__(self, data, scope='planet4'):
+    def __init__(self, data, scope="planet4"):
         self.data = data
         self.scope = scope
 
@@ -58,30 +58,36 @@ class Blotch(Ellipse):
     center : tuple (inherited from matplotlib.Ellipse)
         Coordinates of center, i.e. self.x, self.y
     """
-    to_average = 'x y image_x image_y angle radius_1 radius_2'.split()
 
-    def __init__(self, data, scope='planet4', with_center=False, url_db='', **kwargs):
+    to_average = "x y image_x image_y angle radius_1 radius_2".split()
+
+    def __init__(self, data, scope="planet4", with_center=False, url_db="", **kwargs):
         self.data = data
-        self.scope = scope if scope is not None else 'planet4'
+        self.scope = scope if scope is not None else "planet4"
         self.with_center = with_center
         self.url_db = Path(url_db)
         self.ax = None
-        if scope not in ['hirise', 'planet4']:
-            raise TypeError('Unknown scope: {}'.format(scope))
+        if scope not in ["hirise", "planet4"]:
+            raise TypeError("Unknown scope: {}".format(scope))
         try:
-            self.x = data.x if scope == 'planet4' else data.image_x
-            self.y = data.y if scope == 'planet4' else data.image_y
+            self.x = data.x if scope == "planet4" else data.image_x
+            self.y = data.y if scope == "planet4" else data.image_y
         except AttributeError:
-            print("No x and y attributes in data:\n{}"
-                  .format(data))
+            print("No x and y attributes in data:\n{}".format(data))
             raise AttributeError
         # default member number is 1. This is set to the cluster member inside
         # clustering execution.
         self._n_members = 1
-        super(Blotch, self).__init__((self.x, self.y),
-                                     data.radius_1 * 2, data.radius_2 * 2,
-                                     data.angle, alpha=0.65, linewidth=2,
-                                     fill=False, **kwargs)
+        super(Blotch, self).__init__(
+            (self.x, self.y),
+            data.radius_1 * 2,
+            data.radius_2 * 2,
+            data.angle,
+            alpha=0.65,
+            linewidth=2,
+            fill=False,
+            **kwargs
+        )
         self.data = data
 
     @property
@@ -89,21 +95,23 @@ class Blotch(Ellipse):
         url = io.get_url_for_tile_id(self.data.tile_id)
         return io.get_subframe(url)
 
-    def show_subframe(self, ax=None, aspect='auto'):
+    def show_subframe(self, ax=None, aspect="auto"):
         if ax is None:
             _, ax = plt.subplots(figsize=calc_fig_size(8))
-        ax.imshow(self.subframe, origin='upper', aspect=aspect)
+        ax.imshow(self.subframe, origin="upper", aspect=aspect)
         ax.set_axis_off()
         self.ax = ax
 
     def is_equal(self, other):
-        if self.data.x == other.data.x and\
-           self.data.y == other.data.y and\
-           self.data.image_x == other.data.image_y and\
-           self.data.image_y == other.data.image_y and\
-           self.data.radius_1 == other.data.radius_1 and\
-           self.data.radius_2 == other.data.radius_2 and\
-           self.data.angle == other.data.angle:
+        if (
+            self.data.x == other.data.x
+            and self.data.y == other.data.y
+            and self.data.image_x == other.data.image_y
+            and self.data.image_y == other.data.image_y
+            and self.data.radius_1 == other.data.radius_1
+            and self.data.radius_2 == other.data.radius_2
+            and self.data.angle == other.data.angle
+        ):
             return True
         else:
             return False
@@ -158,13 +166,12 @@ class Blotch(Ellipse):
     def limit_points(self):
         return [self.p1, self.p2, self.p3, self.p4]
 
-    def plot_center(self, ax, color='b'):
-        ax.scatter(self.x, self.y, color=color,
-                   s=20, marker='.')
+    def plot_center(self, ax, color="b"):
+        ax.scatter(self.x, self.y, color=color, s=20, marker=".")
 
-    def plot_limit_points(self, ax, color='b'):
+    def plot_limit_points(self, ax, color="b"):
         for x, y in self.limit_points:
-            ax.scatter(x, y, color=color, s=20, marker='o')
+            ax.scatter(x, y, color=color, s=20, marker="o")
 
     @property
     def n_members(self):
@@ -174,7 +181,7 @@ class Blotch(Ellipse):
     def n_members(self, value):
         self._n_members = value
 
-    def plot(self, color='green', ax=None):
+    def plot(self, color="green", ax=None):
         self.show_subframe(ax)
         ax = self.ax
         if ax is None:
@@ -188,22 +195,22 @@ class Blotch(Ellipse):
     def store(self, fpath=None):
         out = self.data
         for p in range(1, 5):
-            attr = 'p' + str(p)
+            attr = "p" + str(p)
             point = getattr(self, attr)
-            out[attr + '_x'] = point[0]
-            out[attr + '_y'] = point[1]
-        if 'image_id' not in out.index:
+            out[attr + "_x"] = point[0]
+            out[attr + "_y"] = point[1]
+        if "image_id" not in out.index:
             pass
             # out['image_id'] = self.image_id
         if fpath is not None:
-            out.to_hdf(str(fpath.with_suffix('.hdf')), 'df')
-        out['n_members'] = self.n_members
+            out.to_hdf(str(fpath.with_suffix(".hdf")), "df")
+        out["n_members"] = self.n_members
         return out
 
     def __str__(self):
         s = "markings.Blotch object. Input data:\n"
         s += self.data.__str__()
-        s += '\n'
+        s += "\n"
         s += "N_members: {}".format(self.n_members)
         return s
 
@@ -222,8 +229,7 @@ def rotate_vector(v, angle):
         Angle in degrees
     """
     rangle = radians(angle)
-    rotmat = np.array([[cos(rangle), -sin(rangle)],
-                       [sin(rangle), cos(rangle)]])
+    rotmat = np.array([[cos(rangle), -sin(rangle)], [sin(rangle), cos(rangle)]])
     return rotmat.dot(v)
 
 
@@ -264,20 +270,19 @@ class Fan(lines.Line2D):
     base_to_midpoint_vec
     """
 
-    to_average = 'x y image_x image_y angle spread distance'.split()
+    to_average = "x y image_x image_y angle spread distance".split()
 
-    def __init__(self, data, scope='planet4', with_center=False, **kwargs):
+    def __init__(self, data, scope="planet4", with_center=False, **kwargs):
         self.data = data
-        self.scope = scope if scope is not None else 'planet4'
+        self.scope = scope if scope is not None else "planet4"
         self.with_center = with_center
-        if scope not in ['hirise', 'planet4']:
-            raise TypeError('Unknown scope: {}'.format(scope))
+        if scope not in ["hirise", "planet4"]:
+            raise TypeError("Unknown scope: {}".format(scope))
         # first coordinate is the base of fan
-        actual_x = 'x' if scope == 'planet4' else 'image_x'
-        actual_y = 'y' if scope == 'planet4' else 'image_y'
+        actual_x = "x" if scope == "planet4" else "image_x"
+        actual_y = "y" if scope == "planet4" else "image_y"
         try:
-            self.base = self.data.loc[[
-                actual_x, actual_y]].values.astype('float')
+            self.base = self.data.loc[[actual_x, actual_y]].values.astype("float")
         except KeyError:
             print("No x and y in the data:\n{}".format(data))
             raise KeyError
@@ -294,22 +299,27 @@ class Fan(lines.Line2D):
         # second arm
         self.v2 = rotate_vector([self.armlength, 0], beta)
         # vector matrix, stows the 1D vectors row-wise
-        self.coords = np.vstack((self.base + self.v1,
-                                 self.base,
-                                 self.base + self.v2))
+        self.coords = np.vstack((self.base + self.v1, self.base, self.base + self.v2))
         # init fan line, first column are the x-components of the row-vectors
-        lines.Line2D.__init__(self, self.coords[:, 0], self.coords[:, 1],
-                              alpha=0.65, color='white',
-                              **kwargs)
+        lines.Line2D.__init__(
+            self,
+            self.coords[:, 0],
+            self.coords[:, 1],
+            alpha=0.65,
+            color="white",
+            **kwargs
+        )
 
     def is_equal(self, other):
-        if self.data.x == other.data.x and\
-           self.data.y == other.data.y and\
-           self.data.image_x == other.data.image_x and\
-           self.data.image_y == other.data.image_y and\
-           self.data.angle == other.data.angle and\
-           self.data.spread == other.data.spread and\
-           self.data.distance == other.data.distance:
+        if (
+            self.data.x == other.data.x
+            and self.data.y == other.data.y
+            and self.data.image_x == other.data.image_x
+            and self.data.image_y == other.data.image_y
+            and self.data.angle == other.data.angle
+            and self.data.spread == other.data.spread
+            and self.data.distance == other.data.distance
+        ):
             return True
         else:
             return False
@@ -328,9 +338,9 @@ class Fan(lines.Line2D):
 
     @property
     def area(self):
-        tr_h = np.sqrt(self.armlength**2 - self.radius**2)
+        tr_h = np.sqrt(self.armlength ** 2 - self.radius ** 2)
         tr_area = tr_h * self.radius
-        half_circ_area = 0.5 * pi * self.radius**2
+        half_circ_area = 0.5 * pi * self.radius ** 2
         return tr_area + half_circ_area
 
     @property
@@ -352,23 +362,30 @@ class Fan(lines.Line2D):
         "float : for the semi-circle wedge drawing at the end of fan."
         return 0.5 * LA.norm(self.circle_base)
 
-    def add_semicircle(self, ax, color='b'):
+    def add_semicircle(self, ax, color="b"):
         "Draw a semi-circle at end of fan arms using MPL.Wedge."
         # reverse order of arguments for arctan2 input requirements
         theta1 = degrees(arctan2(*self.circle_base[::-1]))
         theta2 = theta1 + 180
-        wedge = mpatches.Wedge(self.semi_circle_center, self.radius, theta1, theta2,
-                               width=0.01 * self.radius, color=color, alpha=0.65)
+        wedge = mpatches.Wedge(
+            self.semi_circle_center,
+            self.radius,
+            theta1,
+            theta2,
+            width=0.01 * self.radius,
+            color=color,
+            alpha=0.65,
+        )
         ax.add_patch(wedge)
 
-    def add_mean_wind_pointer(self, ax, color='b', ls='-'):
+    def add_mean_wind_pointer(self, ax, color="b", ls="-"):
         "Draw a thicker mean wind direction pointer for better visibility in plots."
         endpoint = rotate_vector([3 * self.armlength, 0], self.data.angle)
-        coords = np.vstack((self.base,
-                            self.base + endpoint))
+        coords = np.vstack((self.base, self.base + endpoint))
         self.wind_pointer_end = coords[1]
-        pointer = lines.Line2D(coords[:, 0], coords[:, 1],
-                               alpha=0.65, linewidth=1, linestyle=ls)
+        pointer = lines.Line2D(
+            coords[:, 0], coords[:, 1], alpha=0.65, linewidth=1, linestyle=ls
+        )
         pointer.set_color(color)
         ax.add_line(pointer)
 
@@ -390,33 +407,33 @@ class Fan(lines.Line2D):
         As total length, I define the armlength + the radius of the semi-circle
         at the end.
         """
-        mid_point_vec = rotate_vector([0.5 * (self.armlength + self.radius),
-                                       0],
-                                      self.data.angle)
+        mid_point_vec = rotate_vector(
+            [0.5 * (self.armlength + self.radius), 0], self.data.angle
+        )
         return self.base + mid_point_vec
 
-    def plot_center(self, ax, color='b'):
-        ax.scatter(self.center[0], self.center[1], color=color,
-                   s=20, marker='.')
+    def plot_center(self, ax, color="b"):
+        ax.scatter(self.center[0], self.center[1], color=color, s=20, marker=".")
 
     @property
     def base_to_center_vec(self):
         coords = np.vstack((self.base, self.center))
         return coords
 
-    def add_midpoint_pointer(self, ax, color='b', ls='-'):
+    def add_midpoint_pointer(self, ax, color="b", ls="-"):
         coords = self.base_to_center_vec
-        pointer = lines.Line2D(coords[:, 0], coords[:, 1],
-                               alpha=0.65, linewidth=3, linestyle=ls)
+        pointer = lines.Line2D(
+            coords[:, 0], coords[:, 1], alpha=0.65, linewidth=3, linestyle=ls
+        )
         pointer.set_color(color)
         ax.add_line(pointer)
 
     def __str__(self):
         out = "markings.Fan object. Input data:\n"
 
-        out += 'base: {0}\narmlength: {1}\narm1: {2}\narm2: {3}'\
-            .format(self.base, self.armlength, self.base + self.v1,
-                    self.base + self.v2)
+        out += "base: {0}\narmlength: {1}\narm1: {2}\narm2: {3}".format(
+            self.base, self.armlength, self.base + self.v1, self.base + self.v2
+        )
         return out
 
     def __repr__(self):
@@ -425,14 +442,14 @@ class Fan(lines.Line2D):
     def store(self, fpath=None):
         out = self.data
         for i, arm in enumerate([self.v1, self.v2]):
-            out['arm{}_x'.format(i + 1)] = (self.base + arm)[0]
-            out['arm{}_y'.format(i + 1)] = (self.base + arm)[1]
-        if 'image_id' not in out.index:
+            out["arm{}_x".format(i + 1)] = (self.base + arm)[0]
+            out["arm{}_y".format(i + 1)] = (self.base + arm)[1]
+        if "image_id" not in out.index:
             # out['image_id'] = self.image_id
             raise AttributeError("Storage of image_id failed.")
         if fpath is not None:
-            out.to_hdf(str(fpath.with_suffix('.hdf')), 'df')
-        out['n_members'] = self.n_members
+            out.to_hdf(str(fpath.with_suffix(".hdf")), "df")
+        out["n_members"] = self.n_members
         return out
 
     def to_shapely(self):
@@ -452,14 +469,15 @@ class Fan(lines.Line2D):
         numsegments = 100
 
         # The coordinates of the arc
-        theta = np.radians(np.linspace(start_angle, start_angle+180, numsegments))
+        theta = np.radians(np.linspace(start_angle, start_angle + 180, numsegments))
         x = centerx + self.radius * np.cos(theta)
         y = centery + self.radius * np.sin(theta)
 
         arc = geom.LineString(np.column_stack([x, y]))
 
-        rotated = affinity.rotate(arc, self.data.angle,
-                                  origin=tuple(self.semi_circle_center))
+        rotated = affinity.rotate(
+            arc, self.data.angle, origin=tuple(self.semi_circle_center)
+        )
 
         df = pd.DataFrame(np.vstack([self.coords[::-1][:2], np.array(rotated)]))
         return geom.Polygon(df.round(2).drop_duplicates().values)
--- p4tools/plotting.py
+++ p4tools/plotting.py
@@ -11,3 +11,13 @@ def plot_blotches_for_tile(tile_id, ax=None):
     for _, blotch in tile_blotches.iterrows():
         m = markings.Blotch(blotch, with_center=False)
         m.plot(ax=ax)
+
+
+def plot_fans_for_tile(tile_id, ax=None):
+    fans = io.get_fan_catalog()
+    tile_fans = fans.query("tile_id == @tile_id")
+    if ax is None:
+        _, ax = plt.subplots()
+    for _, fan in tile_fans.iterrows():
+        m = markings.Fan(fan, with_center=False)
+        m.plot(ax=ax)
