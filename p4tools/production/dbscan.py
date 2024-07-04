1374bdf0f4b9116796707688df88978b1d8e5161
toIhr <tom.ihro@outlook.com>
2024-07-04 17:25:22 +0200

    fixed error where get_image_name_markings weren't replaced by get_obsid_markings

--- p4tools/production/dbscan.py
+++ p4tools/production/dbscan.py
@@ -321,7 +321,7 @@ class DBScanner:
 
         logger.info("Clustering image_name %s with msf of %f.", image_name, self.msf)
         db = io.DBManager(self.dbname)
-        data = db.get_image_name_markings(image_name)
+        data = db.get_obsid_markings(image_name)
         image_ids = data.image_id.unique()
         logger.debug("Number of image_ids found: %i", len(image_ids))
         for image_id in tqdm(image_ids):
